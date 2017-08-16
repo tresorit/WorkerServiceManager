4d23accecfd12678945c1655d783fd7c6ac4872c
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2017-08-16 13:23:46 +0200

    Updating build

--- lib/types/portHandler.d.ts
+++ lib/types/portHandler.d.ts
@@ -1,13 +1,13 @@
-export interface Port {
-    postMessage(data: any[], transfers?: any[]): any;
+export interface IMessagePort {
     onmessage: (event: MessageEvent) => void | Promise<void>;
+    postMessage(data: any[], transfers?: any[]): any;
 }
 export declare class PortHandler {
     private port;
     private callHandler;
     private deferreds;
     private nextPid;
-    constructor(port: Port);
+    constructor(port: IMessagePort);
     call(service: any, method: any, args: any): Promise<any>;
     setCallHandler(handler: any): void;
     fire(service: any, method: any, args: any): void;
